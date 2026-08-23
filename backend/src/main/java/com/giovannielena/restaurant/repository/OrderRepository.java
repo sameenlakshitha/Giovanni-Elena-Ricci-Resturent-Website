063bc84124d0132a37bc2a1065aeb423861b594c
Sameen Lakshitha <sameendev01@gmail.com>
2026-08-23 07:59:58 +0530

    Add order repository

---
+++ backend/src/main/java/com/giovannielena/restaurant/repository/OrderRepository.java
@@ -0,0 +1,3 @@
+package com.giovannielena.restaurant.repository;
+import com.giovannielena.restaurant.entity.Order; import org.springframework.data.jpa.repository.JpaRepository; import java.util.*;
+public interface OrderRepository extends JpaRepository<Order,Long>{ List<Order> findByUserIdOrderByCreatedAtDesc(Long userId); }
